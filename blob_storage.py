37bba19d4b20dc9ba494387a73ac3cb43da9a5ba
Fathaah <abdulfta12@gmail.com>
2024-03-13 12:19:05 +0100

    blob storage key updated

--- blob_storage.py
+++ blob_storage.py
@@ -4,7 +4,7 @@ from azure.storage.blob import BlobServiceClient
 class az_blob_storage:
     def __init__(self, account_url="https://pixelperfectstorage.blob.core.windows.net/"):
         self.account_url = account_url
-        self.blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=pixelperfectstorage;AccountKey=jap64tPNR04+hMtLUionN1ENv/sv+Bmj1DDrpEEOqWjFdqnE3I//LK9a23t20Ki7x0SLUswXsw4w+AStVLjn1w==;EndpointSuffix=core.windows.net")
+        self.blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=pixelperfectstorage;AccountKey=DPdTiZIGHzTU46lTGlrnHoL6EAyihSzmDGCpNXQnpDtdarmnhkv6XvDXaBE1FMuB/1SxC7P7vurK+AStIQoaGg==;EndpointSuffix=core.windows.net")
 
     # Add an Image to the Blob Storage
     def add_image(self, blob_name, image_data, container_name="images"):
